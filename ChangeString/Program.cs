bbf98fddd0ae861be9b04eb388a1c93d628d1c7d
Daniela Castro <someonealiveh@gmail.com>
2023-03-17 10:36:48 +0000

    Added the command asking prompts

---
+++ ChangeString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ChangeString
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string!");
+            string userString = Console.ReadLine();
+
+            Console.WriteLine("Now input a character!");
+            char userCharacter = Console.ReadKey().KeyChar;
+        }
+    }
+}
