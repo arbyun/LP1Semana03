a6a69aa089ca2f96ac8649407c48108f8fe3fa8f
Daniela Castro <someonealiveh@gmail.com>
2023-03-17 14:21:25 +0000

    Used a loop to prompt the user for each player's powers

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -17,6 +17,19 @@ namespace PlayerPowers
         {
             Console.Write("Enter the total number of players: ");
             int n = int.Parse(Console.ReadLine());
+            
+            Powers[] playerPowers = new Powers[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter powers for player {i + 1}:");
+                Console.Write("Fly (y/n): ");
+                bool hasFly = Console.ReadLine().ToLower() == "y";
+                Console.Write("X-Ray Vision (y/n): ");
+                bool hasXRayVision = Console.ReadLine().ToLower() == "y";
+                Console.Write("Super Strength (y/n): ");
+                bool hasSuperStrength = Console.ReadLine().ToLower() == "y";
+            }
         }
     }
 }
