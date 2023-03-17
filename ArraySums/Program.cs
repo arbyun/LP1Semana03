7070481f945cc1cdbd05642f4dc3d72caf18bbcd
Daniela Castro <someonealiveh@gmail.com>
2023-03-17 10:47:27 +0000

    Created the array with the specified user dimensions

---
+++ ArraySums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArraySums
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the HORIZONTAL length of the array:");
+            int horizontalArray = Console.Read();
+            
+            Console.WriteLine("Please enter the VERTICAL length of the array:");
+            int verticalArray = Console.Read();
+
+            float[,] myArray = new float[horizontalArray, verticalArray];
+        }
+    }
+}
