789d811ca5a1c3811f2c073a6eafb5b439b76611
Daniela Castro <someonealiveh@gmail.com>
2023-03-17 11:52:09 +0000

    Added console prompt and added error msg if user's input is null

--- PlayerPerks/Program.cs
+++ PlayerPerks/Program.cs
@@ -15,7 +15,16 @@ namespace PlayerPerks
     {
         private static void Main(string[] args)
         {
+            PlayerPerks playerPerks = 0;
             
+            Console.WriteLine("Please input your desired perks:");
+            string userInput = Console.ReadLine();
+
+            if (userInput != null && userInput.Length == 0 || userInput == null)
+            {
+                Console.WriteLine("No perks at all!");
+                return;
+            }
         }
     }
 }
