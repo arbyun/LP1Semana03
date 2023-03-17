75f88532177cf3411c34af22d836a5698e46c589
Daniela Castro <someonealiveh@gmail.com>
2023-03-17 14:23:10 +0000

    Printed all players' powers

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -10,14 +10,14 @@ namespace PlayerPowers
         XRayVision = 2,
         SuperStrength = 4
     }
-    
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.Write("Enter the total number of players: ");
             int n = int.Parse(Console.ReadLine());
-            
+
             Powers[] playerPowers = new Powers[n];
 
             for (int i = 0; i < n; i++)
@@ -29,7 +29,36 @@ namespace PlayerPowers
                 bool hasXRayVision = Console.ReadLine().ToLower() == "y";
                 Console.Write("Super Strength (y/n): ");
                 bool hasSuperStrength = Console.ReadLine().ToLower() == "y";
+                
+                playerPowers[i] = Powers.None;
+
+                if (hasFly)
+                {
+                    playerPowers[i] |= Powers.Fly;
+                }
+
+                if (hasXRayVision)
+                {
+                    playerPowers[i] |= Powers.XRayVision;
+                }
+
+                if (hasSuperStrength)
+                {
+                    playerPowers[i] |= Powers.SuperStrength;
+                }
             }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Player {i + 1} powers: {playerPowers[i]}");
+
+                if (playerPowers[i] == (Powers.Fly | Powers.SuperStrength))
+                {
+                    Console.WriteLine("Flying radiation!");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
