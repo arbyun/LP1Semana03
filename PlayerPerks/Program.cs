59cb9895a3760100e9bb8a2b52b999425ed400b5
Daniela Castro <someonealiveh@gmail.com>
2023-03-17 11:51:12 +0000

    Made the PlayerPerks enum

---
+++ PlayerPerks/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlayerPerks
+{
+    [Flags]
+    internal enum PlayerPerks
+    {
+        WaterBreathing = 1 << 0,
+        Stealth = 1 << 1,
+        AutoHeal = 1 << 2,
+        DoubleJump = 1 << 3
+    }
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
