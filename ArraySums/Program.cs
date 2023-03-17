6db297e78af02afc167831bbb04381588173ca5e
Daniela Castro <someonealiveh@gmail.com>
2023-03-17 10:55:40 +0000

    Made a for loop to substitute each i, j in the array with the user's preferred numbers

--- ArraySums/Program.cs
+++ ArraySums/Program.cs
@@ -13,6 +13,18 @@ namespace ArraySums
             int verticalArray = Console.Read();
 
             float[,] myArray = new float[horizontalArray, verticalArray];
+            
+            // Now we ask for the numbers
+            for (int i = 0; i < horizontalArray; i++)
+            {
+                for (int j = 0; j < verticalArray; j++)
+                {
+                    Console.WriteLine($"Current position: [{i}, {j}]\n\n");
+                    Console.WriteLine("Please input a pair of floats into your array:");
+                    myArray[i, j] = Console.Read();
+                    Console.WriteLine("\nDone!");
+                }
+            }
         }
     }
 }
